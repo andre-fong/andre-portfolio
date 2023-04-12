5fc7cb919e166ceb2ba523c0dd3d7615c071503a
Andre Fong <fongandre123@gmail.com>
2023-04-11 23:26:29 -0400

    chore: Add seo meta tags

--- pages/index.tsx
+++ pages/index.tsx
@@ -18,6 +18,7 @@ export default function Home() {
     <>
       <Head>
         <title>Andre Fong</title>
+        {/* Favicons */}
         <link
           rel="apple-touch-icon"
           sizes="180x180"
@@ -39,6 +40,38 @@ export default function Home() {
         <link rel="mask-icon" href="/safari-pinned-tab.svg" color="#5bbad5" />
         <meta name="msapplication-TileColor" content="#da532c" />
         <meta name="theme-color" content="#8b4000" />
+
+        {/* SEO */}
+        <meta name="title" content="Andre Fong" />
+        <meta
+          name="description"
+          content="Andre Fong is a full stack engineer developing with React's Next.js and working at Verto Health; specializing in automated testing with Cypress."
+        />
+        <meta
+          name="keywords"
+          content="andre fong, andre fong computer science, andre fong UofT, andre fong UTSC, andre fong website, andre fong portfolio"
+        />
+        <meta name="robots" content="index, follow" />
+        <meta httpEquiv="Content-Type" content="text/html; charset=utf-8" />
+        <meta name="language" content="English" />
+
+        {/* OG */}
+        <meta property="og:type" content="website" />
+        <meta property="og:url" content="https://andrefong.com/" />
+        <meta property="og:title" content="Andre Fong" />
+        <meta
+          property="og:description"
+          content="Andre Fong is a full stack engineer developing with React's Next.js and working at Verto Health; specializing in automated testing with Cypress."
+        />
+
+        {/* Twitter */}
+        <meta property="twitter:card" content="summary_large_image" />
+        <meta property="twitter:url" content="https://andrefong.com/" />
+        <meta property="twitter:title" content="Andre Fong" />
+        <meta
+          property="twitter:description"
+          content="Andre Fong is a full stack engineer developing with React's Next.js and working at Verto Health; specializing in automated testing with Cypress."
+        />
       </Head>
 
       <main className={`${styles.content} ${inter.className}`}>
