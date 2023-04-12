4b95549c8cb56061d094efb9b9876bbea6f34ab3
Andre Fong <fongandre123@gmail.com>
2023-04-11 20:34:57 -0400

    chore: Style home buttons and Verto link

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,9 +1,11 @@
 import Head from "next/head";
 import Link from "next/link";
-import Image from "next/image";
-import { Inter, Comfortaa, PT_Sans_Narrow } from "next/font/google";
+import { Inter, PT_Sans_Narrow } from "next/font/google";
 import styles from "@/styles/Home.module.scss";
 import Greeting from "@/components/Greeting";
+import PersonIcon from "@mui/icons-material/Person";
+import ComputerIcon from "@mui/icons-material/Computer";
+import WorkIcon from "@mui/icons-material/Work";
 
 const inter = Inter({ subsets: ["latin"] });
 const ptSansNarrow = PT_Sans_Narrow({
@@ -24,20 +26,36 @@ export default function Home() {
             <Greeting />, I{"'"}M ANDRE!
           </div>
           <div className={`${styles.description} ${ptSansNarrow.className}`}>
-            I{"'"}M A FULL STACK DEVELOPER CURRENTLY WORKING @ VERTO HEALTH.
+            I{"'"}M A FULL STACK DEVELOPER CURRENTLY WORKING @{" "}
+            <span className={styles.verto}>
+              <a
+                href="https://verto.health/"
+                target="_blank"
+                rel="noopener noreferrer"
+              >
+                VERTO HEALTH
+              </a>
+            </span>
+            .
           </div>
         </div>
         <div className={styles.navigation}>
           <Link href="/about">
-            <div className={styles.button}></div>
+            <div className={styles.button} title="About Me">
+              <PersonIcon fontSize="inherit" />
+            </div>
           </Link>
 
           <Link href="/projects">
-            <div className={styles.button}></div>
+            <div className={styles.button} title="My Projects">
+              <ComputerIcon fontSize="inherit" />
+            </div>
           </Link>
 
           <Link href="/experience">
-            <div className={styles.button}></div>
+            <div className={styles.button} title="My Experience">
+              <WorkIcon fontSize="inherit" />
+            </div>
           </Link>
         </div>
       </main>
