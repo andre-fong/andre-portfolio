23742aadd17b6d0160591f68a1708a7e0856dbd5
Andre Fong <fongandre123@gmail.com>
2023-04-13 01:40:36 -0400

    fix: Prevent Nextjs removing styles on prod

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -4,8 +4,11 @@ import Header from "@/components/Header";
 import "@/styles/globals.scss";
 import type { AppProps } from "next/app";
 import { AnimatePresence } from "framer-motion";
+import { useNextCssRemovalPrevention } from "@/utils/useNextCssRemovalPrevention";
 
 export default function App({ Component, pageProps, router }: AppProps) {
+  useNextCssRemovalPrevention();
+
   return (
     <>
       <ThemeProvider>
