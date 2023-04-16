d269e01379641c5ec4eeb4a9a266e9239cb10104
Andre Fong <fongandre123@gmail.com>
2023-04-16 16:48:26 -0400

    chore: Create boilerplate layout for experience

--- pages/experience/verto/index.tsx
+++ pages/experience/verto/index.tsx
@@ -3,6 +3,12 @@ import { motion } from "framer-motion";
 import styles from "@/styles/ExperienceDetails.module.scss";
 import { useBackgroundColor } from "@/components/Background";
 import Head from "next/head";
+import Link from "next/link";
+import { Inter } from "next/font/google";
+import ArrowBackIosIcon from "@mui/icons-material/ArrowBackIos";
+import Image from "next/image";
+
+const inter = Inter({ subsets: ["latin"] });
 
 export default function Verto() {
   useBackgroundColor("rgb(80, 60, 152)");
@@ -73,8 +79,46 @@ export default function Verto() {
         />
       </Head>
 
-      <div className={styles.content}>
-        <div></div>
+      <Link href="/experience">
+        <div className={`${styles.back} ${inter.className}`}>
+          <ArrowBackIosIcon fontSize="large" />
+          <div className={styles.back_text}>Back to Experience</div>
+        </div>
+      </Link>
+
+      <div className={`${styles.container} ${inter.className}`}>
+        <div className={styles.content}>
+          <h1 className={styles.title}>Verto Health</h1>
+          <div className={styles.subtitle}>Jr. QA Automation Developer</div>
+          <p className={styles.paragraph}>
+            In the winter of 2023, I started my first internship at{" "}
+            <a href="https://verto.health/">Verto Health</a>, a healthcare tech
+            startup based in Toronto.
+          </p>
+
+          <div className={styles.picture_container}>
+            <div className={styles.picture}>
+              <Image
+                src="/verto-group-picture.jpg"
+                alt="New hires for Verto, 2023"
+                fill
+                style={{ objectFit: "cover", objectPosition: "top" }}
+              />
+            </div>
+            <div className={styles.caption}>
+              Group photo of new hires at Verto as of 2023 Jan!
+            </div>
+          </div>
+
+          <div className={styles.section_heading}>Responsibilties</div>
+          <p className={styles.paragraph}></p>
+
+          <div className={styles.section_heading}>Innovation Projects</div>
+          <p className={styles.paragraph}></p>
+
+          <div className={styles.section_heading}>Development</div>
+          <p className={styles.paragraph}></p>
+        </div>
       </div>
     </>
   );
