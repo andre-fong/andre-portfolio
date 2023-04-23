b2e9f2cf30c72fec20a14e2af17d0958e4da5d54
Andre Fong <fongandre123@gmail.com>
2023-04-22 22:39:26 -0400

    feat: Render hamburger menu on smaller screens

--- components/Header.tsx
+++ components/Header.tsx
@@ -1,10 +1,10 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import styles from "@/styles/Header.module.scss";
 import { PT_Sans_Narrow } from "next/font/google";
 import Link from "next/link";
 import Image from "next/image";
 import { useRouter } from "next/router";
-import { motion, AnimatePresence } from "framer-motion";
+import { motion, AnimatePresence, useAnimate } from "framer-motion";
 import GitHubIcon from "@mui/icons-material/GitHub";
 import LinkedInIcon from "@mui/icons-material/LinkedIn";
 import EmailIcon from "@mui/icons-material/Email";
@@ -17,9 +17,51 @@ const ptSansNarrow = PT_Sans_Narrow({
 export default function Header() {
   const router = useRouter();
   const [dropdownOpen, setDropdownOpen] = useState<boolean>(false);
+  const [navMenuOpen, setNavMenuOpen] = useState<boolean>(false);
+  const [scope, animate] = useAnimate();
+
+  useEffect(() => {
+    if (navMenuOpen) {
+      animate(
+        "#line_1",
+        { rotateZ: "-45deg", y: 9 },
+        { duration: 0.3, ease: "easeOut" }
+      );
+      animate(
+        "#line_2",
+        { width: 0, opacity: 0 },
+        { duration: 0.3, ease: "easeOut" }
+      );
+      animate(
+        "#line_3",
+        { rotateZ: "45deg", y: -9 },
+        { duration: 0.3, ease: "easeOut" }
+      );
+    } else {
+      animate(
+        "#line_1",
+        { rotateZ: "0deg", y: 0 },
+        { duration: 0.3, ease: "easeOut" }
+      );
+      animate(
+        "#line_2",
+        { width: 30, opacity: 1 },
+        { duration: 0.3, ease: "easeOut" }
+      );
+      animate(
+        "#line_3",
+        { rotateZ: "0deg", y: 0 },
+        { duration: 0.3, ease: "easeOut" }
+      );
+    }
+  }, [navMenuOpen, animate]);
 
   function toggleDropdown() {
     setDropdownOpen((prev) => !prev);
+  }
+
+  function toggleNavMenu() {
+    setNavMenuOpen((prev) => !prev);
   }
 
   const actionsContainer = {
@@ -43,187 +85,293 @@ export default function Header() {
   };
 
   return (
-    <div className={`${styles.content} ${ptSansNarrow.className}`}>
-      <div className={styles.logo}>
-        <motion.div
-          className={styles.picture_container}
-          whileHover={{
-            scale: 1.2,
-            transition: {
-              type: "spring",
-              duration: 0.15,
-            },
-          }}
-          whileTap={{ scale: 1.1, transition: { duration: 0.1 } }}
-        >
+    <>
+      <div className={`${styles.content} ${ptSansNarrow.className}`}>
+        <div className={styles.logo}>
           <motion.div
-            className={styles.picture}
-            initial={{ scale: 1.0, rotateZ: 0 }}
-            animate={{ rotateZ: [10, -10, 10, -10, 10, 0] }}
-            transition={{
-              duration: 0.5,
-              ease: "easeOut",
-              repeat: Infinity,
-              repeatDelay: 4,
+            className={styles.picture_container}
+            whileHover={{
+              scale: 1.2,
+              transition: {
+                type: "spring",
+                duration: 0.2,
+              },
             }}
-            onClick={toggleDropdown}
+            whileTap={{ scale: 1.1, transition: { duration: 0.1 } }}
           >
-            <Image
-              src="/logo.jpg"
-              fill
-              sizes="50px"
-              alt="Andre Fong"
-              priority
+            <motion.div
+              className={styles.picture}
+              initial={{ scale: 1.0, rotateZ: 0 }}
+              animate={{ rotateZ: [10, -12, 15, -12, 10, 0] }}
+              transition={{
+                duration: 0.5,
+                ease: "easeOut",
+                repeat: Infinity,
+                repeatDelay: 4,
+              }}
+              onClick={toggleDropdown}
+            >
+              <Image
+                src="/logo.jpg"
+                fill
+                sizes="50px"
+                alt="Andre Fong"
+                priority
+              />
+            </motion.div>
+
+            <motion.div
+              className={styles.round_border_1}
+              initial={{ opacity: 0 }}
+              animate={{ opacity: [0, 0.2, 0] }}
+              transition={{
+                duration: 0.5,
+                ease: "linear",
+                repeat: Infinity,
+                repeatDelay: 4,
+              }}
+            />
+            <motion.div
+              className={styles.round_border_2}
+              initial={{ opacity: 0 }}
+              animate={{ opacity: [0, 0.2, 0] }}
+              transition={{
+                duration: 0.5,
+                ease: "linear",
+                repeat: Infinity,
+                repeatDelay: 4,
+              }}
+            />
+            <motion.div
+              className={styles.round_border_3}
+              initial={{ opacity: 0 }}
+              animate={{ opacity: [0, 0.2, 0] }}
+              transition={{
+                duration: 0.5,
+                ease: "linear",
+                repeat: Infinity,
+                repeatDelay: 4,
+              }}
+            />
+            <motion.div
+              className={styles.round_border_4}
+              initial={{ opacity: 0 }}
+              animate={{ opacity: [0, 0.2, 0] }}
+              transition={{
+                duration: 0.5,
+                ease: "linear",
+                repeat: Infinity,
+                repeatDelay: 4,
+              }}
             />
           </motion.div>
 
-          <motion.div
-            className={styles.round_border_1}
-            initial={{ opacity: 0 }}
-            animate={{ opacity: [0, 0.2, 0] }}
-            transition={{
-              duration: 0.5,
-              ease: "linear",
-              repeat: Infinity,
-              repeatDelay: 4,
-            }}
-          />
-          <motion.div
-            className={styles.round_border_2}
-            initial={{ opacity: 0 }}
-            animate={{ opacity: [0, 0.2, 0] }}
-            transition={{
-              duration: 0.5,
-              ease: "linear",
-              repeat: Infinity,
-              repeatDelay: 4,
-            }}
-          />
-          <motion.div
-            className={styles.round_border_3}
-            initial={{ opacity: 0 }}
-            animate={{ opacity: [0, 0.2, 0] }}
-            transition={{
-              duration: 0.5,
-              ease: "linear",
-              repeat: Infinity,
-              repeatDelay: 4,
-            }}
-          />
-          <motion.div
-            className={styles.round_border_4}
-            initial={{ opacity: 0 }}
-            animate={{ opacity: [0, 0.2, 0] }}
-            transition={{
-              duration: 0.5,
-              ease: "linear",
-              repeat: Infinity,
-              repeatDelay: 4,
-            }}
-          />
-        </motion.div>
-
-        <AnimatePresence>
-          {dropdownOpen && (
-            <motion.div
-              className={styles.dropdown}
-              initial={{ opacity: 0, height: 0 }}
-              animate={{ opacity: 1, height: 210 }}
-              transition={{
-                duration: 0.3,
-                ease: "easeOut",
-              }}
-              exit={{ opacity: 0, height: 0, mode: "wait" }}
-            >
-              <motion.ol
-                className={styles.actions}
-                variants={actionsContainer}
-                initial="hidden"
-                animate="visible"
+          <AnimatePresence>
+            {dropdownOpen && (
+              <motion.div
+                className={styles.dropdown}
+                initial={{ opacity: 0, height: 0 }}
+                animate={{ opacity: 1, height: 210 }}
+                transition={{
+                  duration: 0.3,
+                  ease: "easeOut",
+                }}
+                exit={{ opacity: 0, height: 0, mode: "wait" }}
               >
-                <motion.li className={styles.action} variants={actionContainer}>
-                  <a
-                    href="https://github.com/andre-fong"
-                    target="_blank"
-                    rel="noreferrer"
+                <motion.ol
+                  className={styles.actions}
+                  variants={actionsContainer}
+                  initial="hidden"
+                  animate="visible"
+                >
+                  <motion.li
+                    className={styles.action}
+                    variants={actionContainer}
                   >
-                    <GitHubIcon fontSize="inherit" />
-                  </a>
-                </motion.li>
+                    <a
+                      href="https://github.com/andre-fong"
+                      target="_blank"
+                      rel="noreferrer"
+                    >
+                      <GitHubIcon fontSize="inherit" />
+                    </a>
+                  </motion.li>
 
-                <motion.li className={styles.action} variants={actionContainer}>
-                  <a
-                    href="https://www.linkedin.com/in/andre-fong/"
-                    target="_blank"
-                    rel="noreferrer"
+                  <motion.li
+                    className={styles.action}
+                    variants={actionContainer}
                   >
-                    <LinkedInIcon fontSize="inherit" />
-                  </a>
-                </motion.li>
+                    <a
+                      href="https://www.linkedin.com/in/andre-fong/"
+                      target="_blank"
+                      rel="noreferrer"
+                    >
+                      <LinkedInIcon fontSize="inherit" />
+                    </a>
+                  </motion.li>
 
-                <motion.li className={styles.action} variants={actionContainer}>
-                  <a
-                    href="mailto:andre.fong@mail.utoronto.ca"
-                    target="_blank"
-                    rel="noreferrer"
+                  <motion.li
+                    className={styles.action}
+                    variants={actionContainer}
                   >
-                    <EmailIcon fontSize="inherit" />
-                  </a>
-                </motion.li>
-              </motion.ol>
-            </motion.div>
-          )}
-        </AnimatePresence>
+                    <a
+                      href="mailto:andre.fong@mail.utoronto.ca"
+                      target="_blank"
+                      rel="noreferrer"
+                    >
+                      <EmailIcon fontSize="inherit" />
+                    </a>
+                  </motion.li>
+                </motion.ol>
+              </motion.div>
+            )}
+          </AnimatePresence>
+        </div>
+        <div className={styles.links}>
+          <Link href="/">
+            <div
+              className={
+                router.pathname === "/"
+                  ? `${styles.link} ${styles.visited}`
+                  : styles.link
+              }
+            >
+              HOME
+            </div>
+          </Link>
+
+          <Link href="/about">
+            <div
+              className={
+                router.pathname === "/about"
+                  ? `${styles.link} ${styles.visited}`
+                  : styles.link
+              }
+            >
+              ABOUT
+            </div>
+          </Link>
+
+          <Link href="/projects">
+            <div
+              className={
+                router.pathname === "/projects"
+                  ? `${styles.link} ${styles.visited}`
+                  : styles.link
+              }
+            >
+              PROJECTS
+            </div>
+          </Link>
+
+          <Link href="/experience">
+            <div
+              className={
+                router.pathname === "/experience"
+                  ? `${styles.link} ${styles.visited}`
+                  : styles.link
+              }
+            >
+              EXPERIENCE
+            </div>
+          </Link>
+        </div>
+        <button
+          className={styles.hamburger_container}
+          onClick={toggleNavMenu}
+          ref={scope}
+        >
+          <div className={styles.line_1} id="line_1" />
+          <div className={styles.line_2} id="line_2" />
+          <div className={styles.line_3} id="line_3" />
+        </button>
       </div>
-      <div className={styles.links}>
-        <Link href="/">
-          <div
-            className={
-              router.pathname === "/"
-                ? `${styles.link} ${styles.visited}`
-                : styles.link
-            }
-          >
-            HOME
-          </div>
-        </Link>
 
-        <Link href="/about">
-          <div
-            className={
-              router.pathname === "/about"
-                ? `${styles.link} ${styles.visited}`
-                : styles.link
-            }
+      <AnimatePresence>
+        {navMenuOpen && (
+          <motion.div
+            className={styles.nav_menu_container}
+            initial={{ x: "100%" }}
+            animate={{ x: 0 }}
+            exit={{ x: "100%", mode: "wait" }}
+            transition={{ duration: 0.5, ease: [0.8, 0, 0.23, 1] }}
           >
-            ABOUT
-          </div>
-        </Link>
+            <div className={`${styles.nav_menu} ${ptSansNarrow.className}`}>
+              <motion.div
+                className={styles.link_bg}
+                initial={{ backgroundPosition: "100%" }}
+                whileHover={{
+                  backgroundPosition: "0%",
+                  transition: { duration: 0.4, ease: "easeOut" },
+                }}
+              >
+                <Link
+                  href="/"
+                  onClick={() => {
+                    setNavMenuOpen(false);
+                  }}
+                >
+                  <h1 className={styles.name}>ANDRE FONG</h1>
+                </Link>
+              </motion.div>
 
-        <Link href="/projects">
-          <div
-            className={
-              router.pathname === "/projects"
-                ? `${styles.link} ${styles.visited}`
-                : styles.link
-            }
-          >
-            PROJECTS
-          </div>
-        </Link>
+              <motion.div
+                className={styles.link_bg}
+                initial={{ backgroundPosition: "100%" }}
+                whileHover={{
+                  backgroundPosition: "0%",
+                  transition: { duration: 0.4, ease: "easeOut" },
+                }}
+              >
+                <Link
+                  href="/about"
+                  onClick={() => {
+                    setNavMenuOpen(false);
+                  }}
+                >
+                  <h2 className={styles.nav_link}>ABOUT</h2>
+                </Link>
+              </motion.div>
 
-        <Link href="/experience">
-          <div
-            className={
-              router.pathname === "/experience"
-                ? `${styles.link} ${styles.visited}`
-                : styles.link
-            }
-          >
-            EXPERIENCE
-          </div>
-        </Link>
-      </div>
-    </div>
+              <motion.div
+                className={styles.link_bg}
+                initial={{ backgroundPosition: "100%" }}
+                whileHover={{
+                  backgroundPosition: "0%",
+                  transition: { duration: 0.4, ease: "easeOut" },
+                }}
+              >
+                <Link
+                  href="/projects"
+                  onClick={() => {
+                    setNavMenuOpen(false);
+                  }}
+                >
+                  <h2 className={styles.nav_link}>PROJECTS</h2>
+                </Link>
+              </motion.div>
+
+              <motion.div
+                className={styles.link_bg}
+                initial={{ backgroundPosition: "100%" }}
+                whileHover={{
+                  backgroundPosition: "0%",
+                  transition: { duration: 0.4, ease: "easeOut" },
+                }}
+              >
+                <Link
+                  href="/experience"
+                  onClick={() => {
+                    setNavMenuOpen(false);
+                  }}
+                >
+                  <h2 className={styles.nav_link}>EXPERIENCE</h2>
+                </Link>
+              </motion.div>
+            </div>
+          </motion.div>
+        )}
+      </AnimatePresence>
+    </>
   );
 }
